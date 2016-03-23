389c6c132b01e04a9588ed84b1e73b2feeab6d24
김한준 <gkswns04@naver.com>
2016-03-23 17:11:24 +0900

    study - 3/23

--- src/step22/exam01/Test08.java
+++ src/step22/exam01/Test08.java
@@ -1,5 +1,6 @@
 // 주제: 주어진 폴더를 뒤져서 중첩 클래스 파일만 이름을 출력하라.
-// > java -cp bin step22.exam01.Test08 ./bin ../project01/bin
+// > java -cp bin step22.exam01.Test08 ./bin
+
 // 출력 결과:
 // /home/bitcamp/git/Java80/java01/bin/step22/exam01/Test07$1.class
 // /home/bitcamp/git/Java80/java01/bin/step22/exam01/Test07$2.class
@@ -14,14 +15,16 @@ import java.io.FileFilter;
 
 public class Test08 {
   public static void main(String[] args) throws IOException {
-    File f = new File(".");
-
-    printFilename(f, ".class");
+    File f = null;
+    for (String path : args) {
+      f = new File(path);
+      printFilename(f, ".class");
+    }
   }
 
   static void printFilename(File f, String ext) throws IOException {
     if (f.isFile()) {
-      if (f.getName().endsWith(ext)) {
+      if (f.getName().endsWith(ext) && f.getName().indexOf('$') >= 0) {
         System.out.println(f.getCanonicalPath());
       }
     } else {
