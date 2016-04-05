77afd99185ca13f0cea487eec64901e9f48115fa
김한준 <gkswns04@naver.com>
2016-04-05 17:09:11 +0900

    study 4/5

---
+++ src/step28/project.sql
@@ -0,0 +1,30 @@
+create table PROJECTS (
+  PNO     int primary key auto_increment,
+  TITLE   varchar(200) not null,
+  SDT     datetime not null,
+  EDT     datetime not null,
+  DESCT   text,
+  STAT    int default 0
+);
+
+create table MEMBERS (
+  MNO     int primary key auto_increment,
+  MNAME   varchar(50) not null,
+  EMAIL   varchar(100) not null,
+  PWD     varchar(10) not null,
+  TEL     varchar(30)
+);
+
+insert into MEMBERS(MNAME,EMAIL,PWD)
+values('홍길동','hong@test.com','1111');
+
+insert into MEMBERS(MNAME,EMAIL,PWD)
+values('임꺽정','leem@test.com','1111');
+
+insert into MEMBERS(MNAME,EMAIL,PWD)
+values('이순신','lee@test.com','1111');
+
+select * from MEMBERS;
+
+
+
